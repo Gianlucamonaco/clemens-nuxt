ad35b8a065a62792e2e3114a51e605649476bb51
Gianluca Monaco <mckenzie.gian@gmail.com>
2023-10-25 11:30:07 +0200

    Cleanup

--- components/Intro/Setting.vue
+++ components/Intro/Setting.vue
@@ -2,7 +2,7 @@
 import { WIDTH_UNIT } from '@/data/constants';
 
 // Preload intro sound
-const introSound = process.client ? useHomepage().sounds?.[0] : null;
+const introSound = process.client && useHomepage ? useHomepage().sounds?.[0] : null;
 if (introSound) useLoadAudio([introSound.url]);
 </script>
 
