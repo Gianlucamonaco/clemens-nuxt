1a9a3d0be7a3f3455b16968a5f137c15603e3845
Gianluca Monaco <mckenzie.gian@gmail.com>
2023-10-25 07:48:33 +0200

    Redirect path "/panel" to server url

--- middleware/redirects.global.ts
+++ middleware/redirects.global.ts
@@ -2,4 +2,9 @@ export default defineNuxtRouteMiddleware((to) => {
   if (to.path === '/home') {
     return navigateTo('/', { redirectCode: 302 })
   }
+
+  if (to.path === '/panel') {
+    const runtimeConfig = useRuntimeConfig();
+    return navigateTo(`${runtimeConfig.app.KIRBY_BASE_URL}/panel`, { external: true, redirectCode: 302})
+  }
 })
